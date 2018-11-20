28ba27ee43e5e05854055301b2bea12c9a2b3ffd
tayloa <halfnote1004@gmail.com>
2018-11-20 12:48:15 -0500

    Remoeved unnecessary php

--- index.php
+++ index.php
@@ -1,45 +1,11 @@
 <?php
-	// if (isset($_POST["preferences"])) {
-	// 	// Get input values from form
-	// 	// TODO: make input strings safe
-	// 	$message = $_POST["course1"];
-	// 	echo "<script type='text/javascript'>alert('$message');</script>";
-	// 	// $email = $_POST["email"];
-	// 	// $first_name = $_POST["first_name"];
-	// 	// $last_name = $_POST["last_name"];
-	// 	// $password = $_POST["password"];
-	// 	// $school = $_POST["school"];
-	// 	//
-	// 	// // TODO: Ensure all fields are filled out
-	// 	// if (createUser($email, $first_name, $last_name, $password, $school)) {
-	// 	// 	// TODO: Redirect new user to dashboard and have them signed in.
-	// 	// }
-	// 	if(isset($_POST['update'])) {
-  //       $dbhost = 'localhost:3036';
-  //       $dbuser = 'root';
-  //       $dbpass = 'rootpassword';
-  //
-  //       $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-  //
-  //       if(! $conn ) {
-  //          die('Could not connect: ' . mysql_error());
-  //       }
-  //
-  //       $emp_id = $_POST['emp_id'];
-  //       $emp_salary = $_POST['emp_salary'];
-  //
-  //       $sql = "UPDATE employee ". "SET emp_salary = $emp_salary ".
-  //          "WHERE emp_id = $emp_id" ;
-  //       mysql_select_db('test_db');
-  //       $retval = mysql_query( $sql, $conn );
-  //
-  //       if(! $retval ) {
-  //          die('Could not update data: ' . mysql_error());
-  //       }
-  //       echo "Updated data successfully\n";
-  //
-  //       mysql_close($conn);
-	// }
+	if (isset($_POST["choices"])) {
+		// Get input values from form
+		// TODO: make input strings safe
+		$message = $_POST["difficulty"];
+    $message = $_POST["score"];
+		echo "<script type='text/javascript'>alert('$message');</script>";
+	}
 ?>
 
 <style>
